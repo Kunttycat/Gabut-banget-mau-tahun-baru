7e2f0cf6c161e5ca17b788b134b8c2618fd5308c
Kuntycat <kunty5274@gmail.com>
2024-12-28 16:44:37 +0700

    Time: 34 ms (40%), Space: 157.5 MB (11.43%) - LeetHub

--- 0018-4sum/0018-4sum.dart
+++ 0018-4sum/0018-4sum.dart
@@ -1,15 +1,15 @@
 class Solution {
   List<List<int>> fourSum(List<int> nums, int target) {
-    // Urutkan nums terlebih dahulu
+    
     nums.sort();
     List<List<int>> result = [];
     
     for (int i = 0; i < nums.length - 3; i++) {
-      // Hindari duplikat untuk elemen pertama
+
       if (i > 0 && nums[i] == nums[i - 1]) continue;
 
       for (int j = i + 1; j < nums.length - 2; j++) {
-        // Hindari duplikat untuk elemen kedua
+
         if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
         int left = j + 1;
@@ -20,9 +20,8 @@ class Solution {
 
           if (sum == target) {
             result.add([nums[i], nums[j], nums[left], nums[right]]);
-            // Hindari duplikat untuk elemen ketiga
+
             while (left < right && nums[left] == nums[left + 1]) left++;
-            // Hindari duplikat untuk elemen keempat
             while (left < right && nums[right] == nums[right - 1]) right--;
 
             left++;
