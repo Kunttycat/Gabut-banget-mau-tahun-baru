2b3f5e22538a159570defe85d37dca63e1b637f8
Kuntycat <kunty5274@gmail.com>
2024-12-29 07:05:09 +0700

    Time: 0 ms (100%), Space: 152.1 MB (9.58%) - LeetHub

--- 0027-remove-element/0027-remove-element.dart
+++ 0027-remove-element/0027-remove-element.dart
@@ -1,12 +1,11 @@
 class Solution {
   int removeElement(List<int> nums, int val) {
     int j = 0;
-    for (int n in nums){
-        if(n != val){
-            nums[j++] = n;
-        }
+    for (int n in nums) {
+      if (n != val) {
+        nums[j++] = n; 
+      }
     }
-    return j;  
+    return j;
   }
-
 }
